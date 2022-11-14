6189920249258d0563e5f91402f7cf1c6b714b48
schabibi1 <arisagse@gmail.com>
2022-11-14 15:26:03 +0100

    add pan gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,50 @@
 import { View, Image } from 'react-native';
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedGestureHandler,
+  useAnimatedStyle
+} from 'react-native-reanimated';
+
+const AnimatedView = Animated.createAnimatedComponent(View);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const onDrag = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.translateX = translateX.value;
+      context.translateY = translateY.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+      translateY.value = event.translationY + context.translateY;
+    },
+  });
+
+  const containerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: translateX.value,
+        },
+        {
+          translateY: translateY.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
-    </View>
+    <PanGestureHandler onGestureEvent={onDrag}>
+      <AnimatedView style={[containerStyle, { top: -350 }]}>
+        <Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={{ width: imageSize, height: imageSize }}
+        />
+      </AnimatedView>
+    </PanGestureHandler>
   );
 }
